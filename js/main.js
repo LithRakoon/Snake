aa1a81931365a6e8c8c7491befea8e59c1d7ab4a
Lithium/Helium <lithiumturkin@protonmail.com>
2022-03-02 09:19:37 +0100

    Bees

---
+++ js/main.js
@@ -0,0 +1,14 @@
+let epic = document.querySelector('body')
+
+epic.addEventListener('keydown', (event) => {
+    if (event.key === "w") {
+        console.log("joe biden")
+    } if (event.key === "a") {
+        console.log("jee")
+    } if (event.key === "s") {
+        console.log("jaa")
+    } if (event.key === "d") {
+        console.log("jaf")
+    }
+});
+
